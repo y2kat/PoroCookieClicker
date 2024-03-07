bb26e98a55467ad282ca14c93cba74f7c9f82003
y2kat <lentacomputadora@gmail.com>
2024-03-06 18:54:10 -0600

    Cookie Counter

--- Assets/Scripts/CookieClicker.cs
+++ Assets/Scripts/CookieClicker.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using TMPro;
 
 public class CookieClicker : MonoBehaviour
 {
-    public int cookieCount = 0;
+    public Button cookieButton; // botón que representa la galleta
+    public TextMeshProUGUI scoreText; // texto de la UI donde se mostrará el contador
+    private int score = 0; // el contador
+    private float cookiesPerSecond = 0.0f;
 
-    void OnMouseDown()
+    void Start()
     {
-        cookieCount++;
+        cookieButton.onClick.AddListener(IncrementScore);
+    }
+
+    void Update()
+    {
+        cookiesPerSecond = score / Time.time;
+
+        scoreText.text = "Cookies: " + score + "\nCookies per Second: " + cookiesPerSecond.ToString("F2");
+    }
+
+    void IncrementScore()
+    {
+        score++; // incrementa el contador
     }
 }
